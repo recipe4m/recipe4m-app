1d20bc14a0dd79ae433f62bfa7a1823ddd708255
fine <dlgmltjr0925@gmail.com>
2022-01-12 10:57:46 +0900

    feature: React Query Provider 추가

--- App.tsx
+++ App.tsx
@@ -1,21 +1,27 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import { DialogProvider } from '@application/context/DialogContext';
 import Navigation from './src/application/navigation';
 import { Provider } from 'react-redux';
 import React from 'react';
 import StackNavigator from '@navigation/StackNavigator';
 import ThemeView from '@common/component/ThemeView';
 import store from './src/application/store';
-import { DialogProvider } from '@application/context/DialogContext';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <ThemeView>
-        <DialogProvider>
-          <Navigation>
-            <StackNavigator />
-          </Navigation>
-        </DialogProvider>
-      </ThemeView>
+      <QueryClientProvider client={queryClient}>
+        <ThemeView>
+          <DialogProvider>
+            <Navigation>
+              <StackNavigator />
+            </Navigation>
+          </DialogProvider>
+        </ThemeView>
+      </QueryClientProvider>
     </Provider>
   );
 }
