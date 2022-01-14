842e3c0f27f0a0bed3bf988d93d55a919671ca3d
fine <dlgmltjr0925@gmail.com>
2022-01-15 02:58:34 +0900

    feature: development 환경 google sign in library 추가

--- src/presentation/login/ui/LoginScreen.tsx
+++ src/presentation/login/ui/LoginScreen.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+import React, { useEffect } from 'react';
+
+import { GOOGLE_SIGN_IN_CLIENT_ID } from '@env';
 import Regular from '@common/component/text/Regular';
 import { View } from 'react-native';
 
 export default function LoginScreen() {
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log(userInfo);
+    } catch (error: any) {
+      console.log(error);
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
+
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: GOOGLE_SIGN_IN_CLIENT_ID,
+    });
+  }, []);
+
   return (
     <View>
       <Regular>회원가입하고 다양한 레시피를 즐겨보세요</Regular>
+      <GoogleSigninButton onPress={signIn} />
     </View>
   );
 }
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,4 +1,5 @@
 declare module '@env' {
   export const NODE_ENV: 'development' | 'production';
   export const API_BASE_URL: string;
+  export const GOOGLE_SIGN_IN_CLIENT_ID: string;
 }
