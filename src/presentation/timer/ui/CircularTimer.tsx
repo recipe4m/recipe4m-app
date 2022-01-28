84c54ee926f4abf83e205ccf0b2ebcccf3c11ede
fine <dlgmltjr0925@gmail.com>
2022-01-28 16:04:30 +0900

    feature: 남은 시간 애니메이션 스타일 추가

--- src/presentation/timer/ui/CircularTimer.tsx
+++ src/presentation/timer/ui/CircularTimer.tsx
@@ -1,15 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { useSharedValue, withTiming } from 'react-native-reanimated';
+import {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
+import Animated from 'react-native-reanimated';
 import CircularProgressBar from '@common/component/CircularProgressBar';
 import Medium from '@common/component/text/Medium';
 import { NotificationTimer } from '@model/NotificationTimer';
 import { TimerEvent } from '@model/Timer';
+import useTheme from '@common/hook/useTheme';
 
 export default function CircularTimer({ timer }: { timer: NotificationTimer }) {
   const progress = useSharedValue<number>(1);
   const [count, setCount] = useState<number>(-1);
+  const fontValue = useSharedValue<number>(0);
+  const { colors } = useTheme();
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    color: colors.MEDIUM,
+    fontSize: interpolate(fontValue.value, [0, 0.6, 1], [12, 19, 20]),
+    opacity: interpolate(fontValue.value, [0, 0.6, 1], [0.7, 1, 0]),
+  }));
 
   useEffect(() => {
     const handleRun = (e: TimerEvent) => {
@@ -17,7 +32,6 @@ export default function CircularTimer({ timer }: { timer: NotificationTimer }) {
 
       if (e.remainTimeout < 5000) {
         const _count = Math.floor(e.remainTimeout / 1000) + 1;
-        console.log(e.remainTimeout, _count);
         if (count !== _count) setCount(_count);
       }
     };
@@ -27,10 +41,19 @@ export default function CircularTimer({ timer }: { timer: NotificationTimer }) {
     };
   }, [timer, count]);
 
+  useEffect(() => {
+    fontValue.value = 0;
+    fontValue.value = withTiming(1, { duration: 1000 });
+  }, [count]);
+
   return (
     <View style={styles.container}>
       <CircularProgressBar progress={progress} />
-      {count !== -1 && <Medium style={styles.count}>{count}</Medium>}
+      {count !== -1 && (
+        <Animated.Text style={[styles.count, animatedStyle]}>
+          {count}
+        </Animated.Text>
+      )}
     </View>
   );
 }
@@ -45,5 +68,6 @@ const styles = StyleSheet.create({
   },
   count: {
     position: 'absolute',
+    fontWeight: '900',
   },
 });
